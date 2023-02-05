9431f550bd2885262f0768543982fe1a0d781c27
Moe Zalzala| <mohammed27zalzala11@gmail.com>
2023-02-05 04:30:18 +0300

    new list of fake data created and linked with Flutter ListView

--- lib/pages/views/forecast.dart
+++ lib/pages/views/forecast.dart
@@ -33,12 +33,12 @@ class _ForecastState extends State<Forecast> {
                   ),
                   OutlinedButton.icon(
                     style: OutlinedButton.styleFrom(
-                      //backgroundColor: Colors.redAccent,
+                        //backgroundColor: Colors.redAccent,
                         side: const BorderSide(
                             width: 1, color: Color(0x99F0F0F0)),
                         padding: const EdgeInsets.fromLTRB(8, 12, 8, 12)),
                     label:
-                    const Icon(Icons.location_on, color: Color(0xCCF0F0F0)),
+                        const Icon(Icons.location_on, color: Color(0xCCF0F0F0)),
                     icon: const Text(
                       'Baghdad',
                       style: TextStyle(fontSize: 16, color: Color(0xCCF0F0F0)),
@@ -122,7 +122,7 @@ class _ForecastState extends State<Forecast> {
                               width: 34,
                               height: 34,
                               image:
-                              AssetImage('assets/images/wind_speed.png')),
+                                  AssetImage('assets/images/wind_speed.png')),
                         ),
                         SizedBox(
                           height: 7,
@@ -166,56 +166,61 @@ class _ForecastState extends State<Forecast> {
             const SizedBox(height: 65),
             Expanded(
                 child: Padding(
-                  padding: const EdgeInsets.fromLTRB(8, 0, 8, 0),
-                  child: ListView.builder(
-                    scrollDirection: Axis.horizontal,
-                    itemCount: data.listOfWeather.length,
-                    itemBuilder: (context, index) {
-                      return Container(
-                        margin: const EdgeInsets.all(8),
-                        decoration: BoxDecoration(
-                            border: Border.all(
-                                width: 2, color: const Color(0xD9F0F0F0)),
-                            borderRadius:
+              padding: const EdgeInsets.fromLTRB(8, 0, 8, 0),
+              child: ListView.builder(
+                scrollDirection: Axis.horizontal,
+                itemCount: data.weatherForCast['Sunday']!.length,
+                //data.listOfWeather.length,
+                itemBuilder: (context, index) {
+                  return Container(
+                    margin: const EdgeInsets.all(8),
+                    decoration: BoxDecoration(
+                        border: Border.all(
+                            width: 2, color: const Color(0xD9F0F0F0)),
+                        borderRadius:
                             const BorderRadius.all(Radius.circular(10))),
-                        child: Padding(
-                          padding: const EdgeInsets.fromLTRB(26, 19, 26, 19),
-                          child: Column(
-                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                            children: [
-                              Text(
-                                data.listOfWeather[index]['hour'],
-                                style: const TextStyle(
-                                  fontSize: 16,
-                                  color: Color(0xD9F0F0F0),
-                                ),
-                              ),
-                              const SizedBox(
-                                height: 14,
-                              ),
-                              Opacity(
-                                opacity: 0.85,
-                                child: Image(
-                                    height: 34,
-                                    width: 34,
-                                    image: AssetImage(
-                                        data.listOfWeather[index]['weather condition'])),
-                              ),
-                              const SizedBox(
-                                height: 14,
-                              ),
-                              Text(
-                                data.listOfWeather[index]['temperature'],
-                                style: const TextStyle(
-                                    fontSize: 28, color: Color(0xD9F0F0F0)),
-                              )
-                            ],
+                    child: Padding(
+                      padding: const EdgeInsets.fromLTRB(26, 19, 26, 19),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: [
+                          Text(
+                            data.weatherForCast['Sunday']![index]['hour'],
+                            //data.listOfWeather[index]['hour'],
+                            style: const TextStyle(
+                              fontSize: 16,
+                              color: Color(0xD9F0F0F0),
+                            ),
                           ),
-                        ),
-                      );
-                    },
-                  ),
-                )),
+                          const SizedBox(
+                            height: 14,
+                          ),
+                          Opacity(
+                            opacity: 0.85,
+                            child: Image(
+                                height: 34,
+                                width: 34,
+                                image: AssetImage(
+                                  data.weatherForCast['Sunday']![index]
+                                      ['weather condition'],
+                                )),
+                          ),
+                          const SizedBox(
+                            height: 14,
+                          ),
+                          Text(
+                            data.weatherForCast['Sunday']![index]
+                                ['temperature'],
+                            style: const TextStyle(
+                                fontSize: 28, color: Color(0xD9F0F0F0)),
+                          )
+                        ],
+                      ),
+                    ),
+                  );
+                },
+              ),
+            )),
             const SizedBox(
               height: 50,
             ),
