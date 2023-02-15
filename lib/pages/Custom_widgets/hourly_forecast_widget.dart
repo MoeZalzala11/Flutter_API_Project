100257fb78caaa9f32dd4a3f484ca9fda78a9953
Moe Zalzala| <mohammed27zalzala11@gmail.com>
2023-02-15 20:46:56 +0300

    ListView.Builder in the main class was extracted into HourlyForecastWidget and linked with the fake data

---
+++ lib/pages/Custom_widgets/hourly_forecast_widget.dart
@@ -0,0 +1,66 @@
+import 'package:flutter/material.dart';
+
+class HourlyForecastWidget extends StatelessWidget {
+  const HourlyForecastWidget(
+      {Key? key,
+      required this.map
+      })
+      : super(key: key);
+
+  final List<Map<String, dynamic>>? map;
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+        child: Padding(
+      padding: const EdgeInsets.fromLTRB(8, 0, 8, 0),
+          child: ListView.builder(
+            scrollDirection: Axis.horizontal,
+            itemCount: map?.length,
+            itemBuilder: (context, index) {
+              return Container(
+                margin: const EdgeInsets.all(8),
+                decoration: BoxDecoration(
+                    border: Border.all(width: 2, color: const Color(0xD9F0F0F0)),
+                    borderRadius: const BorderRadius.all(Radius.circular(10))),
+                child: Padding(
+                  padding: const EdgeInsets.fromLTRB(26, 19, 26, 19),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      Text(
+                        map![index]['hour'],
+                        style: const TextStyle(
+                          fontSize: 16,
+                          color: Color(0xD9F0F0F0),
+                        ),
+                      ),
+                      const SizedBox(
+                        height: 14,
+                      ),
+                      Opacity(
+                        opacity: 0.85,
+                        child: Image(
+                            height: 34,
+                            width: 34,
+                            image: AssetImage(
+                                map![index]['weather condition'])
+                            )
+                      ),
+                      const SizedBox(
+                        height: 14,
+                      ),
+                      Text(
+                        map![index]['temperature'],
+                        style:
+                            const TextStyle(fontSize: 28, color: Color(0xD9F0F0F0)),
+                      )
+                    ],
+                  ),
+                ),
+              );
+            },
+          ),
+    ));
+  }
+}
--- lib/pages/views/forecast.dart
+++ lib/pages/views/forecast.dart
@@ -1,5 +1,6 @@
 import 'package:api_project/fake_data/fake_data.dart';
 import 'package:api_project/pages/Custom_widgets/extra_info_widget.dart';
+import 'package:api_project/pages/Custom_widgets/hourly_forecast_widget.dart';
 import 'package:flutter/material.dart';
 import 'dart:developer' as dev;
 import '../Custom_widgets/main_widget.dart';
@@ -33,6 +34,7 @@ class _ForecastState extends State<Forecast> {
                     'Greetings Moe',
                     style: TextStyle(fontSize: 16, color: Color(0xCCF0F0F0)),
                   ),
+
                   OutlinedButton.icon(
                     style: OutlinedButton.styleFrom(
                         //backgroundColor: Colors.redAccent,
@@ -50,6 +52,7 @@ class _ForecastState extends State<Forecast> {
                 ],
               ),
             ),
+
             Padding(
               padding: const EdgeInsets.fromLTRB(16, 30, 16, 34),
               child: Row(
@@ -74,74 +77,29 @@ class _ForecastState extends State<Forecast> {
                         )),
                   ]),
             ),
+
             MainWidget(
               weatherIcon: data.weatherForCast['Sunday']![0]
                   ['weather condition'],
               temperature: data.weatherForCast['Sunday']![0]['temperature'],
             ),
+
             ExtraInfoWidget(
               humidityPercentage: data.weatherForCast['Sunday']![0]['humidity'],
               windSpeed: data.weatherForCast['Sunday']![0]['wind speed'],
               chanceToRain: data.weatherForCast['Sunday']![0]['chance to rain'],
             ),
+
             const SizedBox(height: 65),
             Expanded(
                 child: Padding(
                   padding: const EdgeInsets.fromLTRB(8, 0, 8, 0),
-                  child: ListView.builder(
-                    scrollDirection: Axis.horizontal,
-                    itemCount: data.weatherForCast['Sunday']!.length,
-                    //data.listOfWeather.length,
-                    itemBuilder: (context, index) {
-                      return Container(
-                        margin: const EdgeInsets.all(8),
-                        decoration: BoxDecoration(
-                            border: Border.all(
-                                width: 2, color: const Color(0xD9F0F0F0)),
-                            borderRadius:
-                                const BorderRadius.all(Radius.circular(10))),
-                        child: Padding(
-                          padding: const EdgeInsets.fromLTRB(26, 19, 26, 19),
-                          child: Column(
-                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                            children: [
-                              Text(
-                                data.weatherForCast['Sunday']![index]['hour'],
-                                //data.listOfWeather[index]['hour'],
-                                style: const TextStyle(
-                                  fontSize: 16,
-                                  color: Color(0xD9F0F0F0),
-                                ),
-                              ),
-                              const SizedBox(
-                                height: 14,
-                              ),
-                              Opacity(
-                                opacity: 0.85,
-                                child: Image(
-                                    height: 34,
-                                    width: 34,
-                                    image: AssetImage(
-                                      data.weatherForCast['Sunday']![index]
-                                          ['weather condition'],
-                                    )),
-                              ),
-                              const SizedBox(
-                                height: 14,
-                              ),
-                              Text(
-                                data.weatherForCast['Sunday']![index]
-                                    ['temperature'],
-                                style: const TextStyle(
-                                    fontSize: 28, color: Color(0xD9F0F0F0)),
-                              )
-                            ],
-                          ),
-                        ),
-                      );
-                    },
-                  ),
-                )),
+
+                  child: HourlyForecastWidget(map: data.weatherForCast['Sunday'],),
+
+
+              )
+            ),
             const SizedBox(
               height: 50,
             ),
